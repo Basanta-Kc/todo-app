d8e0fda153b3ecd316939cc671f4ac87315d9b28
Basanta-Kc <devbasanta@gmail.com>
2023-03-08 22:11:32 +0545

    add todo status

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const todoInputForm = document.getElementById("todo-input-form");
 const addTodoBtn = document.getElementById("add-todo");
 const todoContainer = document.getElementById("todo-container");
 const alertMessage = document.getElementById("alert-message");
-const todos = JSON.parse(localStorage.getItem("todos")) ?? [
-  { value: "learn html", status: "DONE" },
-];
+const todos = JSON.parse(localStorage.getItem("todos")) ?? [];
 // const todos = ["learn thml", "learn css"]
 // const todos = [
 //   { value: "learn html", status: "In Progress" },
@@ -14,15 +12,23 @@ const todos = JSON.parse(localStorage.getItem("todos")) ?? [
   /* <span class="badge bg-info me-auto ms-1">${todos[i].status}</span>
           <button type="button" class="btn btn-primary me-1" data-index="${i}">Mark As Completed</button> */
 }
+
 const renderTodos = () => {
   let todoContainerHtml = "";
 
-  // use array ko reduce method
   for (let i = 0; i < todos.length; i++) {
+    const { value, status } = todos[i];
+    const bagdeClass = status === "Done" ? "bg-success" : "bg-info";
+    const actionButton =
+      status === "Done"
+        ? ""
+        : `<button type="button" class="btn btn-primary me-1">Mark As Completed</button>`;
     todoContainerHtml += `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-          ${todos[i]} 
-          <button type="button" class="btn btn-danger" data-index="${i}">Delete</button>
+        <li class="list-group-item d-flex justify-content-between align-items-center" data-index=${i}>
+          ${value} 
+          <span class="badge ${bagdeClass} me-auto ms-1">${status}</span>
+         ${actionButton}
+          <button type="button" class="btn btn-danger">Delete</button>
         </li>
 `;
   }
@@ -38,7 +44,7 @@ addTodoBtn.addEventListener("click", () => {
     alertMessage.style.display = "block";
   } else {
     alertMessage.style.display = "none";
-    todos.push(newTodo);
+    todos.push({ value: newTodo, status: "In Progress" });
     localStorage.setItem("todos", JSON.stringify(todos));
     renderTodos();
     todoInputForm.value = "";
@@ -47,14 +53,24 @@ addTodoBtn.addEventListener("click", () => {
 });
 
 todoContainer.addEventListener("click", (e) => {
+  const index = e.target.parentElement.dataset.index;
+
   if (e.target.className == "btn btn-danger") {
-    const todoToBeRemovedIndex = e.target.dataset.index;
-    todos.splice(todoToBeRemovedIndex, 1);
+    todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    renderTodos();
+  }
+
+  if (e.target.className == "btn btn-primary me-1") {
+    todos[index].status = "Done";
     localStorage.setItem("todos", JSON.stringify(todos));
     renderTodos();
   }
 });
 
-// const persons = ["bibek", "basanta"]
+const persons = [
+  { name: "basanta", age: 20 },
+  { name: "bibek", age: 10 },
+];
 
-// const persons = [ { name: bibke , age: 20}, { name: basanta, age : 13}]
+persons[1].name = "Bibek Dhungana";
