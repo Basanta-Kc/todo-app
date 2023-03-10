82b519d6d9fd2b4e035aa20d46cf3264cea5a7f8
Basanta-Kc <devbasanta@gmail.com>
2023-03-10 20:15:27 +0545

    search and edit

--- script.js
+++ script.js
@@ -2,22 +2,25 @@ const todoInputForm = document.getElementById("todo-input-form");
 const addTodoBtn = document.getElementById("add-todo");
 const todoContainer = document.getElementById("todo-container");
 const alertMessage = document.getElementById("alert-message");
+const searchInputForm = document.getElementById("search-input-form");
 const todos = JSON.parse(localStorage.getItem("todos")) ?? [];
-// const todos = ["learn thml", "learn css"]
-// const todos = [
-//   { value: "learn html", status: "In Progress" },
-//   { value: "learn CSS", status: "Done" },
-// ];
-{
-  /* <span class="badge bg-info me-auto ms-1">${todos[i].status}</span>
-          <button type="button" class="btn btn-primary me-1" data-index="${i}">Mark As Completed</button> */
-}
 
-const renderTodos = () => {
+const renderTodos = (filteredTodos) => {
   let todoContainerHtml = "";
 
-  for (let i = 0; i < todos.length; i++) {
-    const { value, status } = todos[i];
+  if(filteredTodos?.length == 0){
+    todoContainer.innerHTML = 
+      `<div class="alert alert-primary" role="alert">
+        No todos found for search term: ${searchInputForm.value}
+      </div>`
+    ;
+    return;
+  }
+
+  const todoToRender = filteredTodos ?? todos
+
+  for (let i = 0; i < todoToRender.length; i++) {
+    const { value, status } = todoToRender[i];
     const bagdeClass = status === "Done" ? "bg-success" : "bg-info";
     const actionButton =
       status === "Done"
@@ -28,6 +31,7 @@ const renderTodos = () => {
           ${value} 
           <span class="badge ${bagdeClass} me-auto ms-1">${status}</span>
          ${actionButton}
+         <button type="button" class="btn btn-secondary me-1">Edit</button>
           <button type="button" class="btn btn-danger">Delete</button>
         </li>
 `;
@@ -38,11 +42,30 @@ const renderTodos = () => {
 
 renderTodos();
 
+// onchange, keypress, keydown
+// then i found i out about search
+// then i found out about input
+searchInputForm.addEventListener("input", (e) => {
+  console.log("rest",searchInputForm.value)
+  const filteredTodos = todos.filter(({value}) => value.includes(e.target.value) )
+  console.log(filteredTodos)
+  renderTodos(filteredTodos)
+})
+
 addTodoBtn.addEventListener("click", () => {
   const newTodo = todoInputForm.value;
   if (newTodo === "") {
     alertMessage.style.display = "block";
   } else {
+    if (todoInputForm.dataset.index) {
+      todos[todoInputForm.dataset.index].value = newTodo;
+      todoInputForm.value = "";
+      todoInputForm.dataset.index = "";
+      localStorage.setItem("todos", JSON.stringify(todos));
+      renderTodos();
+      return;
+    }
+
     alertMessage.style.display = "none";
     todos.push({ value: newTodo, status: "In Progress" });
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -51,6 +74,8 @@ addTodoBtn.addEventListener("click", () => {
     todoInputForm.focus();
   }
 });
+
+
 
 todoContainer.addEventListener("click", (e) => {
   const index = e.target.parentElement.dataset.index;
@@ -66,11 +91,9 @@ todoContainer.addEventListener("click", (e) => {
     localStorage.setItem("todos", JSON.stringify(todos));
     renderTodos();
   }
+
+  if (e.target.className == "btn btn-secondary me-1") {
+    todoInputForm.value = todos[index].value;
+    todoInputForm.dataset.index = index;
+  }
 });
-
-const persons = [
-  { name: "basanta", age: 20 },
-  { name: "bibek", age: 10 },
-];
-
-persons[1].name = "Bibek Dhungana";
